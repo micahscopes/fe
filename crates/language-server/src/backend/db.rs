c7b8ee8bfa4c581cb038aacee5c9818ee6f5febb
Micah <micahscopes@gmail.com>
2024-12-27 05:26:50 -0600

    add stdlib dependencies to all ingots

--- crates/language-server/src/backend/db.rs
+++ crates/language-server/src/backend/db.rs
@@ -1,8 +1,10 @@
-use common::InputDb;
+use common::{InputDb, InputIngot};
 
 use hir::{HirDb, LowerHirDb, SpannedHirDb};
 use hir_analysis::HirAnalysisDb;
 use salsa::{ParallelDatabase, Snapshot};
+
+use super::get_std::get_std_ingot;
 
 #[salsa::jar(db = LanguageServerDb)]
 pub struct Jar(crate::functionality::diagnostics::file_line_starts);
@@ -25,9 +27,29 @@ impl<DB> LanguageServerDb for DB where
     hir_analysis::Jar,
     Jar
 )]
-#[derive(Default)]
 pub struct LanguageServerDatabase {
     storage: salsa::Storage<Self>,
+    std_ingot: Option<InputIngot>,
+}
+
+impl Default for LanguageServerDatabase {
+    fn default() -> Self {
+        let mut db = LanguageServerDatabase {
+            storage: salsa::Storage::default(),
+            std_ingot: None,
+        };
+
+        db.std_ingot = Some(get_std_ingot(&mut db));
+        db
+    }
+}
+
+impl LanguageServerDatabase {
+    pub(crate) fn std_ingot(&self) -> &InputIngot {
+        self.std_ingot
+            .as_ref()
+            .expect("std ingot should have been created at startup")
+    }
 }
 
 impl salsa::Database for LanguageServerDatabase {
@@ -38,6 +60,7 @@ impl ParallelDatabase for LanguageServerDatabase {
     fn snapshot(&self) -> Snapshot<Self> {
         Snapshot::new(LanguageServerDatabase {
             storage: self.storage.snapshot(),
+            std_ingot: self.std_ingot.clone(),
         })
     }
 }
